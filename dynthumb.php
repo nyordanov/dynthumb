1e7d76641056727effc2714b97b3624c69c07560
Nikolay Yordanov <me@nyordanov.com>
2010-09-06 13:27:40 +0300

    option for error reporting in debug mode

--- dynthumb.php
+++ dynthumb.php
@@ -14,5 +14,10 @@
  * @example /dynthumb.php?src=pic.jpg&w=100&h=100&zc=1
  */
 
- 
+define('DEBUG', true); // set to false in production
+
+if(DEBUG == true) {
+	error_reporting(E_ALL);
+	ini_set('display_error', 1);
+}
  
