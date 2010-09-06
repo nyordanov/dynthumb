da9359761138e257c9406716d56a1eaccf4649de
Nikolay Yordanov <me@nyordanov.com>
2010-09-06 13:21:58 +0300

    add phpdoc comment in the beginning of the file

---
+++ dynthumb.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * A library similar to timthumb, but using imagick
+ * 
+ * @author Nikolay Yordanov <me@nyordanov.com> 
+ * @link http://nyordanov.com
+ * 
+ * @param string src
+ * @param int w
+ * @param int h
+ * @param bool zc
+ * 
+ * @example /dynthumb.php?src=pic.jpg&w=100&h=100&zc=1
+ */
+
+ 
+ 
